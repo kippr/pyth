517b7bfa4b4a3ec332504c5dc4d8a805807ad876
Kris Powell <krispowell@avocacapital.com>
2015-12-03 18:17:11 +0000

    Checking document -> RTF works for underline

--- tests/test_writertf15.py
+++ tests/test_writertf15.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from pyth.plugins.xhtml.reader import XHTMLReader
 from pyth.plugins.rtf15.writer import Rtf15Writer
+from pyth.document import Document, Paragraph, Text
 
 class TestRtfWithImage(unittest.TestCase):
 
@@ -13,6 +14,14 @@ class TestRtfWithImage(unittest.TestCase):
             self.assertIn('pngblip', doc)
             self.assertIn('picwgoal750\\', doc)
             self.assertIn('pichgoal750\\', doc)
+
+
+    def test_underline_output(self):
+        text = Text(content=[u'Underlined'], properties={'underline': True})
+        para = Paragraph(content=[text])
+        doc = Document(content=[para])
+        result = Rtf15Writer.write(doc).getvalue()
+        self.assertIn('\\ul Underlined\\ul0', result)
 
 
 if __name__ == '__main__':
